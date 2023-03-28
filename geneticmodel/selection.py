46b6132349d3ca2b1ca9ac947f56dab9ad2dc658
Nanduri Jayant Vishnu <nandurijayant.vishnu2020@vitstudent.ac.in>
2023-03-29 02:02:32 +0530

    final commit

--- geneticmodel/selection.py
+++ geneticmodel/selection.py
@@ -10,7 +10,7 @@ def roulette_wheel_selection(population, fitness_fn, num_parents,data,userID):
     # Calculate the sum of all fitness scores
     total_fitness = sum(fitness_scores)
     # Calculate the relative fitness for each individual
-    relative_fitness = [fitness_score / total_fitness for fitness_score in fitness_scores]
+    relative_fitness = [fitness_score / total_fitness if total_fitness!=0 else 0 for fitness_score in fitness_scores]
 
     # Select the top 'num_parents' individuals using roulette wheel selection
     parents = []
--- geneticmodel/tag_slots.py
+++ geneticmodel/tag_slots.py
@@ -8,6 +8,9 @@ def add_slots(data, data2):
     start = int(data["interval_start"])
 
     tags = data2
+    
     for i in range(len(tags)):
         tags[i]["slots"] = [math.ceil(j/slot_dur) for j in range(int(tags[i]['start_time'])-start, int(tags[i]['end_time'])-start,slot_dur)]
+    print("tags are: ",data2)
+    print()
     return tags
